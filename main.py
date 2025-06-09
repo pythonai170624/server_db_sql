b2ecad4ead5073b192b815aa2992083fff1b72dc
pythonai250824 <pythonai250824@gmail.com>
2025-06-09 20:54:12 +0300

    upload

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ from models import *
 # Create FastAPI instance
 app = FastAPI(title="Example API", description="A simple FastAPI with Swagger UI", version="1.0")
 
-
 @app.get("/items/")
 def get_all():
     result = bl.get_items()
@@ -27,28 +26,3 @@ def read_root():
 def read_root():
     return {"message": "item updated"}
 
-@app.get("/sum")
-def sum_two_get(a: int, b: int):
-    result = bl.sum_two(a, b)
-    return {"sum": result}
-
-class TwoNumbers(BaseModel):
-    a: int
-    b: int
-
-@app.post("/sum-post")
-def sum_two_post(two_numbers: TwoNumbers):
-    result = bl.sum_two(two_numbers.a, two_numbers.b)
-    return {"sum": result}
-
-@app.get("/get-entities")
-def get_entities(text: str):
-    print(f"========= input get_entities: {text}")
-    result = bl.get_entities(text)
-    return {"entities": result}
-
-@app.get("/get-person")
-def get_person(text: str):
-    print(f"========= input get_person: {text}")
-    result = bl.get_person(text)
-    return {"people": result}
